4c67784536cc44d6ffbad5fbe54b62b7583b2a87
ShivamKashyap <shivam.kashyap@newgen.co.in>
2018-10-20 18:29:47 +0530

    Angular with different version setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ladderStoneUI';
-
-  //https://www.youtube.com/watch?v=rajjv-0bSps
+  title = 'app';
 }
